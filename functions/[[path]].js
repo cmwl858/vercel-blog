c101416851d0f2d67fe4c3ae6bb424c8c0183b68
你的名字 <>
2025-03-16 20:52:23 +0800

    Refactor for Cloudflare Pages deployment

---
+++ functions/[[path]].js
@@ -0,0 +1,20 @@
+export async function onRequest(context) {
+  const express = require('express');
+  const path = require('path');
+  const app = express();
+
+  // 设置视图引擎
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, '../views'));
+
+  // 静态文件
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.use(express.urlencoded({ extended: true }));
+
+  // 路由
+  app.get('/', (req, res) => {
+      res.render('index', { title: 'My Blog' });
+  });
+
+  return new Response("Hello World!");
+}
